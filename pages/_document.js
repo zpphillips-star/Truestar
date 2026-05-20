876c1fa4f23ed3637e0c63255f782edfd7f7d01a
Zach Phillips <zaphilli@microsoft.com>
2026-05-20 09:48:54 -0700

    feat: wire Google Analytics G-2EKJ2P5CC3 to all pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GA_ID = "G-2EKJ2P5CC3";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
